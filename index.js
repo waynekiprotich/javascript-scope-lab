ccb5d5f326cc063eb5cad42276bcb492b5fbcc9c
Wayne Kiprotich <wanekip123@icloud.com>
2025-12-16 08:39:29 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+/*Flat Burger Menu*/
+
+//variable data
+let burger = ['humburger', 'cheeseburger'];
+let featuredDrink ='strawberry milkshake';
+
+//function and block
+function addBurger (){
+    let newBurger = 'Flatburger';
+    burger.push(newBurger);
+    console.log(burger);
+}
+
+//Variable Declaration and Scope
+if (true){
+    let anotherNewBurger = 'Maple Bacon Burger';
+    burger.push(anotherNewBurger);
+    console.log(burger);
+}
+
+//change featured drink
+function changeFeaturedDrink(){
+    featuredDrink = 'The JavaShake';
+    console.log(featuredDrink);
+}
